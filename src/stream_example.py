3e445348b95968e0afc2b1910c265682af6148fa
Chase E Stewart <chst9148@colorado.edu>
2017-10-07 06:30:24 +0000

    correct color from the camera

--- src/stream_example.py
+++ src/stream_example.py
@@ -5,16 +5,16 @@ import sys, cv2
 import numpy as np
 
 
+# initialize output HDMI stream
 base = BaseOverlay("base.bit")
-# TODO initialize a input webcam
 my_mode = VideoMode(640, 480, 24)
 hdmi_out = base.video.hdmi_out
-hdmi_out.configure(my_mode, PIXEL_BGR)
+hdmi_out.configure(my_mode, None ) 
 hdmi_out.start()
 print("Initialize output_HDMI")
 
 
-# initialize an output HDMI stream
+# initialize input USB video capture
 video_in = cv2.VideoCapture(0)
 video_in.set(cv2.CAP_PROP_FRAME_WIDTH,  640 )
 video_in.set(cv2.CAP_PROP_FRAME_HEIGHT, 480 )
@@ -23,8 +23,11 @@ print("Initialize video")
 try:
 	print("Initialize video stream!")
 
+	# main loop- grab a frame from webcam, process it, push to HDMI
 	while True:
 		retcode, frame_vga = video_in.read()
+
+		# TODO FIXME process image here 
 		if retcode == True:
 			outframe = hdmi_out.newframe()
 			outframe[0:480, 0:640,:] = frame_vga[0:480,0:640,:]
@@ -38,7 +41,7 @@ try:
 			del video_in
 			sys.exit()
 
-except KeyboardIAP_PROP_FRAME_WIDTH
+except KeyboardInterrupt:
 	print("Goodbye")
 	video_in.release()
 	hdmi_out.stop()
